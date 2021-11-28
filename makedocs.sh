bbb0f8780c0a604668aa2fc86351c7f2e18444bf
Colaski <colaskidev@gmail.com>
2021-11-27 21:45:42 -0800

    The makedocs script seems to be broken
    xcodebuild docbuild seems to be building the documentation wrong somehow, or maybe I'm using it wrong.

--- makedocs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-xcodebuild docbuild -scheme SwAuth-Package -derivedDataPath ./BuildFiles
-cp -R ./BuildFiles/Build/Products/Debug/SwAuth.doccarchive ./docs
-rm -rf ./BuildFiles/Build
-
-echo "Updated docs"
